5c737486494cca9d5e7f9f97bb9c1a9f32e3320d
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-08-29 16:56:24 -0400

    hoisting

---
+++ app.js
@@ -0,0 +1,29 @@
+// const num1 = 30;
+// const num2 = 230;
+
+// function add() {
+//   console.log(`the result is: ${num1 + num2}`);
+// }
+// add();
+// (function () {
+//   const m1 = 300;
+//   const m2 = 530;
+//   console.log(`the result is: ${m1 + m2}`);
+// })();
+
+// console.log(num1);
+
+// // argument
+// (function (m1, m2) {
+//   console.log(`the result is: ${m1 + m2}`);
+// })(10, 900);
+
+// // assign
+// const result = (function (m1, m2) {
+//   console.log(`the result is: ${m1 + m2}`);
+//   return m1 + m2;
+// })(500, 5900);
+// console.log(result);
+
+
+
