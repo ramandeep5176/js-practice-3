de2f9bfd57b19fae371cca047bb05424f8564311
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-08-30 19:46:19 -0400

    done

---
+++ es6/app2.js
@@ -0,0 +1,49 @@
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// var number = 500;
+// console.log(number);
+// number = 10;
+// console.log(number);
+// var number = "org";
+// console.log(number);
+
+// let amo = 150;
+// console.log(amo);
+// amo = 50;
+// console.log(amo);
+// let amo = "so";
+// console.log(amo);
+
+// const tt = 100;
+// console.log(tt);
+// // tt = 88;
+
+// const dd={name:'bobb'}
+// dd.name='ram'
+// console.log(dd.name);
+
+
+// 
+
+var amount =100
+
+function greet (){
+    var random='some ran'
+    console.log(`hello ${amount} ${random}`);
+}
+// console.log();
+// greet ()
+
+var total=1000
+var test=true
+
+if(test){
+    console.log('heel');
+}
