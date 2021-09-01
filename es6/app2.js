0b7401f3ab20fbc5c91dec90cb38a497f07f4ea6
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-09-01 19:20:58 -0400

    tag templeate literal

--- es6/app2.js
+++ es6/app2.js
@@ -31,35 +31,49 @@
 
 //
 
-var amount = 100;
+// var amount = 100;
 
-function greet() {
-  var random = "some ran";
-  console.log(`hello ${amount} ${random}`);
+// function greet() {
+//   var random = "some ran";
+//   console.log(`hello ${amount} ${random}`);
+// }
+// // console.log();
+// // greet ()
+
+// let total = 1000;
+// let test = true;
+
+// if (test) {
+//   let total = "orng & on";
+//   let value='some'
+//   console.log("heel");
+// }
+
+// {
+//   let total = "ring";
+//   console.log(total);
+
+// }
+// {
+// let total = "ring";
+// console.log(total);
+// }
+
+// // console.log(value);
+// console.log(total);
+
+const author = "some author";
+const statement = "some statement";
+
+const quote = highlight`here is ${statement} by ${author}and it's true.`;
+console.log(quote);
+
+const result = document.getElementById("result");
+result.innerHTML = quote;
+
+function highlight(text, ...vars) {
+  const aewsomeText=text.map((item,index)=>{
+      return `${item} <strong class="blue">${vars[index] || ""}</strong>`;
+  })
+  return aewsomeText.join('')
 }
-// console.log();
-// greet ()
-
-let total = 1000;
-let test = true;
-
-if (test) {
-  let total = "orng & on";
-  let value='some'
-  console.log("heel");
-}
-
-{
-  let total = "ring";
-  console.log(total);
-
-}
-{
-let total = "ring";
-console.log(total);
-}
-
-
-
-// console.log(value);
-console.log(total);
