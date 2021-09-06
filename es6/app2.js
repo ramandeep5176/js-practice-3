a1cbbfa0a32bb8e949214b052f1f7cc288cf40ee
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-09-06 11:51:28 -0400

    object destruct

--- es6/app2.js
+++ es6/app2.js
@@ -85,18 +85,16 @@
 // const num=dob(4)
 // // console.log(num);
 
-
-
 // const multiply=(num1,num2) =>{
 //     const result =num1*num2
-//     // 
+//     //
 //     return result
 // }
 
 // const sum=multiply(4,5)
 // // console.log(sum);
 
-// // 
+// //
 // const object =() => ({name:'john',jj:45})
 // const person = object()
 // // console.log(person);
@@ -108,6 +106,58 @@
 // const big= numbers.filter(number => number > 7 )
 // console.log(big);
 
-
 // const btn=document.querySelector('.btn')
 // btn.addEventListener('click',()=>console.log('yccjk'))
+
+// const btn = document.querySelector(".btn");
+
+// btn.addEventListener("click", function () {
+//   console.log(this);
+
+//   setTimeout( ()=> {
+//     this.style.color = "red";
+//   }, 2000);
+// });
+
+// const john = "john";
+// const pete = "pete";
+
+// function sayhi(person = "tim") {
+//   console.log(`hi ${person}`);
+// }
+// const sayhello = (person ='bob') => console.log(`hello ${person}`);
+
+// sayhi();
+// sayhello();
+
+// const fruits=['orange','gg','eh','euid']
+// const friends=['orawfnge','gwfeweg','ehwfwe','wfeweeuid']
+
+// const orange=fruits[0]
+// const gg=fruits[1]
+// const eh=fruits[2]
+// // console.log(orange,gg,eh);
+
+// const [orawfnge,,ehwfwe]=friends
+// console.log(orawfnge, ehwfwe);
+
+// let first = "bob";
+// let second = "john";
+// // let temp = second;
+// // second = first;
+// // first = temp;
+
+// [second,first]=[first,second]
+// console.log(first, second);
+
+// const bio = {
+//   first: "bob",
+//   last: "sam",
+//   city: "chic",
+//   siblings: {
+//     sister: "june",
+//   },
+// };
+
+// const {first:firstname,last,city,siblings:{sister:favs}}=bio
+// console.log(firstname, last, city,favs);
