72406f0af52f470ae7a8a01f2ef01793531aec63
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-09-11 12:43:31 -0400

    async-done

--- es6/app2.js
+++ es6/app2.js
@@ -365,12 +365,22 @@ const heading1 = document.querySelector(".one");
 const heading2 = document.querySelector(".two");
 const heading3 = document.querySelector(".three");
 const btn = document.querySelector(".btn");
-btn.addEventListener("click", () => {
-  addColor(1000, heading1, "red")
-    .then(() => addColor(2000, heading2, "green"))
-    .then(() => addColor(2000, heading3, "blue"))
-    .catch((err) => console.log(err));
+btn.addEventListener("click", async () => {
+  const result = await displayColor();
+  console.log(result);
 });
+
+async function displayColor() {
+  try {
+    const first = await addColor(500, heading1, "red");
+    await addColor(500, heading2, "green");
+    await addColor(500, heading3, "red");
+    console.log(first);
+  } catch (error) {
+    console.log(error);
+  }
+  return "heell";
+}
 
 function addColor(time, element, color) {
   return new Promise((resolve, reject) => {
